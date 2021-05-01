c4c0abf4c93a802cbb3bea6538853f92f811fafa
marcelhfm <hofmann.marcel@hm.edu>
2021-05-01 15:11:05 +0200

    some new tests

--- tests/test_userDB.py
+++ tests/test_userDB.py
@@ -1,5 +1,5 @@
 import json
-
+import pandas as pd
 from pymysql import connections
 from flaskr.dietDB import Database
 import loadConfig
@@ -16,26 +16,36 @@ def connectDB():
     except:
         for path in sys.path:
             print(path)
+            
+def execute_initDB():
+    db = connectDB()
+    
+    db.initDB()
     
 def test_insert_user():
     db = connectDB()
-    user = json.loads('{"username": "test5", "password": "1a4"}')
+    user = json.loads('{"username": "Peter", "password": "supersecret123"}')
 
     db.insertUser(json_data=user)
     
-def test_select_user():
+def test_select_user(username):
     db = connectDB()
-    user = db.selectUser("Mario")
+    user = db.selectUser(username)
     print(user)
     
-def test_select_food():
+def test_select_food(name):
     db = connectDB()
-    food = db.selectFood("Birne")
+    food = db.selectFood(name)
     print(food)
+    
+def test_getUserViaID(id):
+    db = connectDB()
+    user = db.getUserViaID(id)
+    print(user)
 
 def main():
     print("starting testing...")
-    test_select_food()
+    test_getUserViaID("2")
     print("finished testing...")
     
 
